1934de6c3f425d11ffc063257dc7b4b4d2b8c482
ic-434 <akbalinartu@mail.ru>
2023-04-24 16:38:40 +0500

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
+const btn = document.getElementById('nickname');
+const modal = document.getElementById('modal');
 
-  
+const close_modal = document.getElementById('modal_close');
+
+btn.onclick = () => {
+    modal.classList.add('madal_active');
+
+    close_modal.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', hideModal);
+
+    function closeModal() {
+        modal.classList.remove('madal_active');
+        close_modal.removeEventListener('click', hideModal);
+    }
+
+    function hideModal(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    }
+};
