11bfed814886221c2fff310db85294a28e0f3519
ic-434 <akbalinartu@mail.ru>
2023-05-13 21:10:21 +0500

    bobble one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+let bubble_index = document.querySelector('.bubble_index');
+
+let height = bubble_index.getBoundingClientRect().width;
+
+bubble_index.style['height'] = height  + 'px';
